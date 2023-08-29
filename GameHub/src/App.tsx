efcda5ca2b11c1dde04c16964a30096b41fd19bc
smahesh77 <smahesh7736@gmail.com>
2023-08-29 23:23:37 +0530

    setting up layout

--- GameHub/src/App.tsx
+++ GameHub/src/App.tsx
@@ -1,17 +1,32 @@
 import React from "react";
-import {
-
-  Grid,
-  GridItem,
-} from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
+    // template is like the layout of our site
+    // its like we define the areas in the  form rows and columns like matrix
     <div>
-      <Grid templateAreas={`"nav nav" "aside main"`}>
+      <Grid
+        templateAreas={{
+          base: `"nav " " main"`, // we dont want aside for mobile devices,
+          lg: `"nav nav" "aside main"`, // for devices greater than 1024px, this basically defines all the ares of our component
+        }}
+      >
+        <GridItem area="nav" bg={"red"}>
+          {" "}
+          Nav
+        </GridItem>
+         <Show above="lg"> {/*the show will only render the components under it if it is above or below a certain size */}
+          <GridItem area="aside" bg={"gold"}>
+            {" "}
+            Aside
+          </GridItem>
+        </Show>
 
-        <GridItem area={'nav'} bg={"red"}> nav</GridItem>
-
+        <GridItem area="main" bg={"green"}>
+          {" "}
+          Main
+        </GridItem>
       </Grid>
     </div>
   );
